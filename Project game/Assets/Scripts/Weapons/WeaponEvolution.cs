291fa1f75c6ba7cb3ed57b58f5925b65c83d3678
Noppadon <noppadon.pny@gmail.com>
2024-08-20 17:08:51 +0700

    Add Evolution Weapon BaseScript
    Add WeaponEvolution Scriptable Script

---
+++ Project game/Assets/Scripts/Weapons/WeaponEvolution.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu (fileName = "WeaponEvolution" , menuName = "ScriptableObjects/WeaponEvolution")]
+public class WeaponEvolution : ScriptableObject
+{
+    public WeaponScriptableObject WeaponBaseData;
+    public PassiveitemScriptableScript CatalystPassiveItemData;
+    public WeaponScriptableObject EvolutionWeaponData;
+    public GameObject EvoWeapon;
+}
