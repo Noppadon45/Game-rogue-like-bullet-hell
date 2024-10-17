bf3fdac3ac1356c1fb28f53360b61b39c05ab473
Noppadon <noppadon.pny@gmail.com>
2024-10-17 22:08:46 +0700

    Comment in WeaponDataEditor Script

--- Project game/Assets/Scripts/Editor/WeaponDataEditor.cs
+++ Project game/Assets/Scripts/Editor/WeaponDataEditor.cs
@@ -14,30 +14,37 @@ public class WeaponDataEditor : Editor
 
     void OnEnable()
     {
+        //Catch the weapon data value
         weaponData = (WeaponData)target;
 
+        //Retrive all weapon subtype and catch it
         System.Type baseType = typeof(Weapon);
         List<System.Type> subTypes = System.AppDomain.CurrentDomain.GetAssemblies()
             .SelectMany(s => s.GetTypes())
             .Where(p => baseType.IsAssignableFrom(p) && p != baseType)
             .ToList() ;
 
+        //Add a None option in front
         List<string> subTypeString = subTypes.Select(t => t.Name).ToList();
         subTypeString.Insert(0 , "None");
         weaponSubType = subTypeString.ToArray();
 
+        //Ensure the weapon is correct weapon subtype
         selectWeaponSubType = Math.Max(0 , Array.IndexOf(weaponSubType, weaponData.behaviour));
     }
 
     public override void OnInspectorGUI()
     {
+        //Draw a dropdown in the Inspector
         selectWeaponSubType = EditorGUILayout.Popup("Behaviour", Math.Max(0, selectWeaponSubType) , weaponSubType);
 
         if (selectWeaponSubType > 0)
         {
+            //Update the behaviour field
             weaponData.behaviour = weaponSubType[selectWeaponSubType].ToString();
-            EditorUtility.SetDirty(weaponData);
-            DrawDefaultInspector();
+            
+            EditorUtility.SetDirty(weaponData);     //Mark the object to save
+            DrawDefaultInspector();         //Draw the default inspector elements
         }
         
     }
