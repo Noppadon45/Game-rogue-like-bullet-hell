fd66d0aea250cb8854e7c2e62251ec36a5834d51
Noppadon <noppadon.pny@gmail.com>
2024-10-17 17:08:23 +0700

    Add Comment in ProjectileWeapon

--- Project game/Assets/Scripts/Weapons/ProjectileWeapon.cs
+++ Project game/Assets/Scripts/Weapons/ProjectileWeapon.cs
@@ -6,13 +6,14 @@ using UnityEngine.Rendering;
 
 public class ProjectileWeapon : Weapon
 {
-    protected float currentAttackInterval;      //Attack Count Interval
+    protected float currentAttackInterval;      //Attack time Interval
     protected int currentAttackCount;           //Attack Count of weapon
 
     protected override void Update()
     {
         base.Update();
 
+        //if currentAttackInterval > 0 call attack
         if (currentAttackInterval > 0)
         {
             currentAttackInterval -= Time.deltaTime;
@@ -34,26 +35,29 @@ public class ProjectileWeapon : Weapon
 
     protected override bool Attack(int AttackCount = 1)
     {
+        //if no projectilePrefab Show error what prefab has not been set
         if (!currentStats.projectilePrefab)
         {
             Debug.Log(string.Format("Projectile prefab has not been set {0}" , name));
             currentCooldown = data.baseStats.cooldown;
             return false;
         }
-        
+        //Check Weapon can attack or not
         if (!CanAttack())
         {
             return false;
         }
-
+        //Calculate the angle and offset of weapon will spawn projectile
         float spawnAngle = GetSpawnAngle();
 
+        //Spawn a copy of the projectile
         Projectile prefab = Instantiate(currentStats.projectilePrefab, playerStats.transform.position + (Vector3)GetSpawnOffset(spawnAngle) , Quaternion.Euler(0 , 0 , spawnAngle));
 
 
         prefab.weapon = this;
         prefab.player = playerStats;
-
+            
+        //Reset cooldown if this attack using cooldown
         if (currentCooldown <= 0 )
         {
             currentCooldown += currentStats.cooldown;
@@ -61,6 +65,7 @@ public class ProjectileWeapon : Weapon
 
         AttackCount--;
 
+        //Perform next attack
         if (AttackCount > 0)
         {
             currentAttackCount = AttackCount;
@@ -69,12 +74,13 @@ public class ProjectileWeapon : Weapon
         return true;
     }
 
-
+    //Get which direction of the player and spawning projectile should face
     protected virtual float GetSpawnAngle()
     {
         return Mathf.Atan2(playermovement.moveDirection.y , playermovement.moveDirection.x) * Mathf.Rad2Deg;
     }
 
+    //Generate a random point to spawn the projectile on and Rotate the facing of the point by SpawnAngle
     protected virtual Vector2 GetSpawnOffset(float spawnAngle = 0)
     {
         return Quaternion.Euler(0 , 0 , spawnAngle) * new Vector2(
