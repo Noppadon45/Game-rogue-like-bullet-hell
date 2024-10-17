810adc497250825b3f5635918f093bebf68ddb62
Noppadon <noppadon.pny@gmail.com>
2024-10-17 18:54:53 +0700

    Add WeaponDataEditor Script
    for using Behaviour of weapon in Editor of Unity

---
+++ Project game/Assets/Scripts/Editor/WeaponDataEditor.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using System;
+
+public class WeaponDataEditor : Editor
+{
+    WeaponData weaponData;
+    string[] weaponSubType;
+    int selectWeaponSubType;
+
+    void OnEnable()
+    {
+        weaponData = (WeaponData)target;
+
+        System.Type baseType = typeof(Weapon);
+        List<System.Type> subTypes = System.AppDomain.CurrentDomain.GetAssemblies()
+            .SelectMany(s => s.GetTypes())
+            .Where(p => baseType.IsAssignableFrom(p) && p != baseType)
+            .ToList() ;
+
+        List<string> subTypeString = subTypes.Select(t => t.Name).ToList();
+        subTypeString.Insert(0 , "None");
+        weaponSubType = subTypeString.ToArray();
+
+        selectWeaponSubType = Math.Max(0 , Array.IndexOf(weaponSubType, weaponData.behaviour));
+    }
+}
