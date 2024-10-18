3cf45b50263893befe9c943fc24728120dbf85b8
Noppadon <noppadon.pny@gmail.com>
2024-10-19 00:26:04 +0700

    Comment in Passive & PassiveData Script

--- Project game/Assets/Scripts/Passive Item/Passive.cs
+++ Project game/Assets/Scripts/Passive Item/Passive.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//A class take PassiveData and it use in a player stats when receive
 public class Passive : Item
 {
     public PassiveData passiveData;
@@ -15,6 +16,7 @@ public class Passive : Item
         public CharacterData.Stats boosts;
     }
 
+    //create passive and set in initialise
     public virtual void Initialise(PassiveData Data)
     {
         base.Initialise(Data);
@@ -28,16 +30,19 @@ public class Passive : Item
         return currentBoosts;
     }
 
+    //LevelUp the weapon by 1 and calulate the stats with weapon
     public override bool DoLevelUp()
     {
         base.DoLevelUp();
 
+        //When cant levelup when item is max
         if (!CanLevelUp())
         {
             Debug.Log(string.Format("Cant Level Up {0} to Level {1} , max level of {2} already max" , name , currentLevel , maxLevel));
             return false;
         }
-
+        
+        //add stats of the next level to the weapon
         currentBoosts += passiveData.GetLevelData(currentLevel++).boosts;
         return true;
         
--- Project game/Assets/Scripts/Passive Item/PassiveData.cs
+++ Project game/Assets/Scripts/Passive Item/PassiveData.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//A class for store stats of PassiveData in Game
+
+
 [CreateAssetMenu(fileName = ("Passive Data") , menuName =("Passive Data"))]
 public class PassiveData : ItemData
 {
@@ -10,10 +13,12 @@ public class PassiveData : ItemData
 
     public Passive.Modifier GetLevelData(int  level)
     {
+        //Pick the Stats from the next Level
         if (level - 2 < growth.Length)
         {
             return growth[level - 2];
         }
+        //Return an empty value and warning
         Debug.Log(string.Format("Passive dont have level {0} for levle up" , level));
         return new Passive.Modifier();
     }
